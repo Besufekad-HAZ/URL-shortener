f9da17a2705fafd0cf34b60e301c88006b835154
Besufekad-HAZ <besurye@gmail.com>
2024-06-12 23:51:59 +0300

    Work on create method in links controller file

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,4 +2,13 @@ class LinksController < ApplicationController
   def index
     @links = Link.all
   end
+
+  def create
+    @link = Link.new(link_params)
+    if @link.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
 end
