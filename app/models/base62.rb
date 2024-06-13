eb1c72234c4dcc9b09b42aa5c7c703d5bffeb10a
Besufekad-HAZ <besurye@gmail.com>
2024-06-13 18:07:23 +0300

    Create Base62 Model

---
+++ app/models/base62.rb
@@ -0,0 +1,9 @@
+class Base62
+  def self.encode(number)
+    alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  end
+
+  def self.decode(string)
+    alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  end
+end
