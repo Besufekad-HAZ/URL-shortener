7eb444b647b1c26afafd842b683d7e14b98be758
Besufekad-HAZ <besurye@gmail.com>
2024-06-13 18:54:50 +0300

    Add 6 tests

--- test/models/base62_test.rb
+++ test/models/base62_test.rb
@@ -2,14 +2,26 @@ require "test_helper"
 
 class Base62Test < ActiveSupport::TestCase
   test "encode 0" do
-    assert true
+    assert_equal "0", Base62.encode(0)
   end
 
   test "encode 1" do
-    assert true
+    assert_equal "1", Base62.encode(1)
   end
 
   test "encode 10" do
-    assert true
+    assert_equal "a", Base62.encode(10)
+  end
+
+  test "encode 62" do
+    assert_equal "10", Base62.encode(62)
+  end
+
+  test "encode 1024" do
+    assert_equal "gw", Base62.encode(1024)
+  end
+
+  test "decode 999_999" do
+    assert_equal "4c91", Base62.decode("0")
   end
 end
