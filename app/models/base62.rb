df5561f9f8e8a15a112a136596943dbcaad63a08
Besufekad-HAZ <besurye@gmail.com>
2024-06-13 18:12:06 +0300

    Create Base62 test for the model

--- app/models/base62.rb
+++ app/models/base62.rb
@@ -1,9 +1,9 @@
 class Base62
   def self.encode(number)
-    alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    # alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
   end
 
   def self.decode(string)
-    alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    # alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
   end
 end
---
+++ test/models/base62_test.rb
@@ -0,0 +1,15 @@
+require "test_helper"
+
+class Base62Test < ActiveSupport::TestCase
+  test "encode 0" do
+    assert true
+  end
+
+  test "encode 1" do
+    assert true
+  end
+
+  test "encode 10" do
+    assert true
+  end
+end
