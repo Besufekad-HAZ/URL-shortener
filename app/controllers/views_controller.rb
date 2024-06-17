13f951dbd23942ed00f7f92554bed901e3d60ffa
Besufekad-HAZ <besurye@gmail.com>
2024-06-17 20:05:37 +0300

    Work on the chart and use expand range and time intervals for users to check the chart graph

--- app/controllers/views_controller.rb
+++ app/controllers/views_controller.rb
@@ -2,6 +2,7 @@ class ViewsController < ApplicationController
   before_action :set_link
 
   def show
+    @link.views.create!( ip: request.ip, user_agent: request.user_agent)
     redirect_to @link.url, allow_other_host: true
   end
 end
