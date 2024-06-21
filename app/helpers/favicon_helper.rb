3414740cdb5f8d075105492cd7c75519df73a58e
Besufekad-HAZ <besurye@gmail.com>
2024-06-21 16:56:23 +0300

    Include google_favicon_url method in the helpers too

--- app/helpers/favicon_helper.rb
+++ app/helpers/favicon_helper.rb
@@ -2,4 +2,8 @@ module FaviconHelper
   def favicon_image_tag(domain)
     image_tag google_favicon_url(domain)
   end
+
+  def google_favicon_url(domain)
+    "https://www.google.com/s2/favicons?domain=#{domain}"
+  end
 end
