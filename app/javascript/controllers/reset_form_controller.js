6fa3e448c5d6299c50704f0aab6aad9fdb6b7f4c
Besufekad-HAZ <besurye@gmail.com>
2024-06-24 23:42:37 +0300

    Fix some typos and reset form errors

--- app/javascript/controllers/reset_form_controller.js
+++ app/javascript/controllers/reset_form_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   reset() {
-    this.element.reset();
-    this.element.querySelector('input[type="checkbox"]').checked = true;
+    this.element.reset()
+    this.element.querySelector('input:not([type="hidden"])').focus();
   }
 }
