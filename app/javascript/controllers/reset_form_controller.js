ac6e2bdd45592720ae3b1fea0bceb8a987c20f84
Besufekad-HAZ <besurye@gmail.com>
2024-06-24 23:33:33 +0300

    Implement refresh anc select check box

--- app/javascript/controllers/reset_form_controller.js
+++ app/javascript/controllers/reset_form_controller.js
@@ -3,5 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   reset() {
     this.element.reset();
+    this.element.querySelector('input[type="checkbox"]').checked = true;
   }
 }
