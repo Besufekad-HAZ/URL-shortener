1fee7dc465a6c221b1dbacd42ecc4c01c147d181
Besufekad-HAZ <besurye@gmail.com>
2024-06-24 23:44:02 +0300

    Fully implement auto focus using [autofocus='autofocus'] in reset_form_controller

--- app/javascript/controllers/reset_form_controller.js
+++ app/javascript/controllers/reset_form_controller.js
@@ -3,6 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   reset() {
     this.element.reset()
-    this.element.querySelector('input:not([type="hidden"])').focus();
+    this.element.querySelector("[autofocus='autofocus']")?.focus();
   }
 }
