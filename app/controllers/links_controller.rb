5b0812fec1b295a0661eaa175d0e9ba55c72e129
Besufekad-HAZ <besurye@gmail.com>
2024-06-24 23:17:46 +0300

    Add reset form options in links contorller page

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -13,7 +13,11 @@ class LinksController < ApplicationController
     if @link.save
       respond_to do |format|
         format.html { redirect_to root_path }
-        format.turbo_stream { render turbo_stream: turbo_stream.prepend("links", @link) }
+        format.turbo_stream { render turbo_stream: [
+          turbo_stream.prepend("links", @link),
+          turbo_stream.replace("link_form", partial: "links/form", locals: { link: Link.new })
+        ]
+      }
       end
     else
       index
