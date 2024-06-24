6d0a66401eba93d6e7bf973ea18d5710f1b5778c
Besufekad-HAZ <besurye@gmail.com>
2024-06-24 22:44:28 +0300

    Add turbo javascript and broadcast replace to links and metadata job

--- app/jobs/metadata_job.rb
+++ app/jobs/metadata_job.rb
@@ -2,5 +2,6 @@ class MetadataJob < ApplicationJob
   def perform(id)
     link = Link.find(id)
     link.update Metadata.retrieve_from(link.url).attributes
+    link.broadcast_replaced_to (link)
   end
 end
