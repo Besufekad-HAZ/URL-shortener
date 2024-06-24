a56c08e60a7e9c4d7122f268b432ef42d90fed59
Besufekad-HAZ <besurye@gmail.com>
2024-06-24 18:12:05 +0300

    Implement some changes inside the metadata model

--- app/models/metadata.rb
+++ app/models/metadata.rb
@@ -1,10 +1,6 @@
 class Metadata
-  def self.update(link)
-    link.metadata = Metadata.retrieve_from(link.url)
-    link.save
-  end
-
-  def self.retrieve_from(url)
+    def self.retrieve_from(url)
+      new()
     metadata = MetaInspector.new(url)
     {
       title: metadata.title,
