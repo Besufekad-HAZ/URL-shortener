f4c2160da82d542d067f7e7bfa235135251dbb26
Besufekad-HAZ <besurye@gmail.com>
2024-07-01 22:13:43 +0300

    Check the presence or truthiness of user_id

--- app/models/link.rb
+++ app/models/link.rb
@@ -23,7 +23,7 @@ class Link < ApplicationRecord
 
  # Returns false if the link is not owned by the user
  def editable_by?(user)
-  user_id? && (user_id == user.id)
+  user_id? && (user_id == user&.id)
  end
 
 end
