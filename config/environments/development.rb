331f4c7d47ba5ce38f2945957410c21c889c3214
Besufekad-HAZ <besurye@gmail.com>
2024-07-01 16:56:02 +0300

    Generate devise User model and user_test and other neccessary files

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,6 +63,7 @@ Rails.application.configure do
   config.assets.quiet = true
 
   config.active_job.queue_adapter = :sucker_punch
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
