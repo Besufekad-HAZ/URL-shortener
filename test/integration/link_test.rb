1f005ca5b9096785b4f2d412f68fc518051b364c
Besufekad-HAZ <besurye@gmail.com>
2024-07-04 00:23:12 +0300

    Include some more tests

--- test/integration/link_test.rb
+++ test/integration/link_test.rb
@@ -15,4 +15,24 @@ class LinkTest < ActionDispatch::IntegrationTest
   test "cannot edit a link as guest" do
 
   end
+
+  test "cannot edit user's link as guest" do
+
+  end
+
+  test "can edit user's link as user" do
+    user = users(:one)
+    sign_in_as user
+    link = links(:one)
+    get edit_link_path(link)
+    assert_response :success
+  end
+
+  test "cannot edit another user's link" do
+    user = users(:one)
+    sign_in_as user
+    link = links(:one)
+    patch link_path(link), params: { link: { url: "https://www.google.com" } }
+    assert_equal "https://www.google.com", link.reload.url
+  end
 end
