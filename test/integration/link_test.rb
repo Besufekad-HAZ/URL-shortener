892646580dd558fee3f3fd77ea94e3e56b6b34fb
Besufekad-HAZ <besurye@gmail.com>
2024-07-04 00:21:02 +0300

    Write sample starting tets

--- test/integration/link_test.rb
+++ test/integration/link_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class LinkTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "create link as guest" do
+
+  end
+
+  test "create link as user" do
+    user = users(:one)
+    sign_in_as user
+    post links_path, params: { link: { url: "https://www.google.com" } }
+    assert_equal 1, Link.count
+  end
+
+  test "cannot edit a link as guest" do
+
+  end
 end
