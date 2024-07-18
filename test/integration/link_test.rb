b5b5fa0f2eb02d85f54b527fa18acf9ec3d70512
Besufekad-HAZ <besurye@gmail.com>
2024-07-18 16:57:43 +0300

    Update the link_test.rb file with latest changes

--- test/integration/link_test.rb
+++ test/integration/link_test.rb
@@ -1,28 +1,27 @@
 require "test_helper"
 
 class LinkTest < ActionDispatch::IntegrationTest
-
   test "create link requires url" do
     post links_path, params: { link: { url: "" } }
     assert_response :unprocessable_entity
   end
 
   test "create link as guest" do
-   assert_difference "Link.count" do
-    post links_path(format: :turbo_stream), params: { link: { url: "https://www.google.com" } }
-    assert_response :ok
-    assert_nil Link.last.user_id
-   end
+    assert_difference "Link.count" do
+      post links_path(format: :turbo_stream), params: { link: { url: "https://www.google.com" } }
+      assert_response :ok
+      assert_nil Link.last.user_id
+    end
   end
 
   test "create link as user" do
     user = users(:one)
     sign_in user
     assert_difference "Link.count" do
-    post links_path(format: :turbo_stream), params: { link: { url: "https://www.google.com" } }
-    assert_response :ok
-    assert_equal user.id, Link.last.user_id
-  end
+      post links_path(format: :turbo_stream), params: { link: { url: "https://www.google.com" } }
+      assert_response :ok
+      assert_equal user.id, Link.last.user_id
+    end
   end
 
   test "guest cannot edit link" do
@@ -36,9 +35,9 @@ class LinkTest < ActionDispatch::IntegrationTest
   end
 
   test "users can edit their own link" do
-   sign_in users(:one)
-   get edit_link_path(links(:one))
-   assert_response :ok
+    sign_in users(:one)
+    get edit_link_path(links(:one))
+    assert_response :ok
   end
 
   test "user cannot edit another user's link" do
