2776405fa9c54ee175d1c60d445fb5880288095a
Besufekad-HAZ <besurye@gmail.com>
2024-07-19 12:31:16 +0300

    Implement some styliing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,23 @@
 module ApplicationHelper
   include Pagy::Frontend
+
   def pagy_nav_tailwind(pagy)
     html = []
     pagy.series.each do |item|
       case item
       when Integer
-        html << link_to(item, url_for(page: item), class: "px-4 py-2 border border-gray-300 bg-white text-gray-700 hover:bg-gray-100 rounded-md")
+        # Check if the item is the current page
+        if item == pagy.page
+          # Current page styles
+          html << link_to(item, url_for(page: item), class: "px-4 py-2 border border-gray-300 bg-black text-white hover:bg-gray-700 rounded-md")
+        else
+          # Other page styles
+          html << link_to(item, url_for(page: item), class: "px-4 py-2 border border-gray-300 bg-black text-gray-700 hover:bg-gray-100 rounded-md")
+        end
       when 'gap'
-        html << content_tag(:span, '…', class: "px-4 py-2 border border-gray-300 bg-white text-gray-700")
+        html << content_tag(:span, '…', class: "px-4 py-2 border border-gray-300 bg-black text-gray-700")
       else
+        # Apply a different style if needed for the prev/next or disabled buttons
         html << content_tag(:span, item, class: "px-4 py-2 border border-gray-300 bg-white text-gray-700")
       end
     end
